d7c22af67436dca5368129eaa7770d1130d4e25f
Harsh K. Nagarkar <hknagark@go.olemiss.edu>
2019-08-31 18:23:45 -0500

    saving untracked

--- webpack/test.webpackconfig.js
+++ webpack/test.webpackconfig.js
@@ -10,13 +10,14 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\.css$/i,
+                test: /\.css$/,
                 use: [
                     'style-loader',
                     'css-loader',
                 ]
-
-            }
+            },
+            { test: /\.(png|woff|woff2|eot|ttf|svg)$/, loader: 'url-loader?limit=100000' }
         ]
+
     }
 };
--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,44 +1,23 @@
-var path =  require("path");
+var path = require('path');
 var webpack = require('webpack');
 module.exports = {
-    entry: [
-        '/app.js',
-        // '/webpack/src/js/jquery-3.2.1.min.js',
-        // '/webpack/src/js/bootstrap-4.1/popper.min.js',
-        // '/webpack/src/js/bootstrap-4.1/bootstrap.min.js',
-        // '/webpack/src/js/slick/slick.min.js',
-        // '/webpack/src/js/wow/wow.min.js',
-        // '/webpack/src/js/animsition/animsition.min.js',
-        // '/webpack/src/js/bootstrap-progressbar/bootstrap-progressbar.min.js',
-        // '/webpack/src/js/jquery.waypoints.min.js',
-        // '/webpack/src/js/jquery.counterup.min.js',
-        // '/webpack/src/js/circle-progress/circle-progress.min.js',
-        // '/webpack/src/js/perfect-scrollbar/perfect-scrollbar.js',
-        // '/webpack/src/js/chartjs/Chart.bundle.min.js',
-        // '/webpack/src/js/select2/select2.min.js'
-
-    ],
+    mode: 'development',
+    entry: './app.js',
     output: {
-        path  : path.resolve(__dirname, 'webpack/dist'),
-        filename: 'bundle.js',
-        publicPath: '/webpack/dist'
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
     },
-    module:{
+    module: {
         rules: [
             {
                 test: /\.css$/,
-                user: [
+                use: [
                     'style-loader',
-                    'css-loader'
+                    'css-loader',
                 ]
-
-            }
+            },
+            { test: /\.(png|woff|woff2|eot|ttf|svg)$/, loader: 'url-loader?limit=100000' }
         ]
-    },
-    plugins: [
-        new webpack.optimizer.UglifyJsPlugin({
-            
 
-        })
-    ]
+    }
 };
